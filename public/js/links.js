9e2f2019525e1a0cb8d11160e266306dae8596e7
Alvaro-004 <alvaro051@gmail.com>
2025-05-02 08:52:49 -0300

    img da pagina

--- public/js/links.js
+++ public/js/links.js
@@ -1,5 +1,6 @@
 const LINKS = {
-  AGENDAMENTO: "https://wa.me/qr/RZREFPGKWXAJC1",
+  AGENDAMENTO:
+    "https://api.whatsapp.com/send/?phone=5591991124716&text=%EF%BF%BD+Ol%C3%A1%2C+vim+pelo+site.+Gostaria+de+conversar+sobre+servi%C3%A7os+automotivos&type=phone_number&app_absent=0",
 };
 
 document.querySelectorAll(".btn-agendar").forEach((button) => {
